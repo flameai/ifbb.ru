3b7db4dd134440f11db5b2b88c4434ba69154f96
Андрюков Александр Леонидович <andryukov@worldgym.burg>
2019-03-05 17:44:24 +0500

    calend

--- ifbb/mainapp/admin.py
+++ ifbb/mainapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+
+from .models import Slider
+
+from .models import calend_type
+
+
+admin.site.register(Slider)
+
+admin.site.register(calend_type)
--- ifbb/mainapp/models.py
+++ ifbb/mainapp/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Slider(models.Model):
     image = models.ImageField(verbose_name=u'Изображение')
     title_internal = models.CharField(verbose_name=u'Название', max_length=200, help_text=u"Отображается только в адм.части")
+    title_external = models.CharField(verbose_name=u'Оглавление на сайте', max_length=200, help_text=u"Отображается на сайте",default='')
+    date_str=models.CharField(verbose_name=u'ДатаПроведения', max_length=40, help_text=u"Отображается на сайте под Оглавлением",default='')
+
     button_url = models.URLField(verbose_name=u'Ссылка', blank=True)
     order = models.PositiveIntegerField(default=0)
 
@@ -13,6 +16,22 @@ class Slider(models.Model):
         verbose_name = u'слайдер'
         verbose_name_plural = u'слайдеры'
 
+    def __str__(self):
+        return "%s" % self.title_internal
+
+    def __unicode__(self):
+        return "%s" % self.title_internal
+
+
+
+class calend_type(models.Model):
+    image = models.ImageField(verbose_name=u'Изображение')
+    title_internal = models.CharField(verbose_name=u'Название', max_length=200, help_text=u"Отображается только в адм.части")
+
+    class Meta:
+        verbose_name = u'Тип события в календаре'
+        verbose_name_plural = u'Типы событий в календаре'
+    
     def __str__(self):
         return "%s" % self.title_internal
 
